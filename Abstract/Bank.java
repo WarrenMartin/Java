f530beae2c1ae5109a95d43953c5014cf77b5748
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-04-14 11:56:56 -0400

    Update Bank.java

--- Abstract/Bank.java
+++ Abstract/Bank.java
@@ -1,11 +1,30 @@
 // now to use the abstract ATM we extend bank class
-
 public class Bank extends ATM{
-    
-  public void withdraw(){
-  System.out.println("Withdraw success");
-}
-  public void deposit(){
-    System.out.println("Money Deposited");
-  }
+
+	public Bank(double intrest,int cashback){
+		super(intrest,cashback);
+	}
+
+	public void withdraw(){
+		System.out.println("Withdrawend");
+	}
+
+	public void deposit(){
+		System.out.println("Deposited");
+	}
+
+	@Override
+	public void disp(){
+		super.disp(); // will call parent class disp method
+		System.out.println("I am Bank");
+	}
+	
+	public static void main(String[] args){
+		ATM a1=new Bank(5,1);
+		a1.disp();
+		
+		//Bank b1=new Bank(5,11); // you get the same ans only diff is ATM is specifier and Bank is a implementor
+		//b1.disp();
+	}
+	
 }
