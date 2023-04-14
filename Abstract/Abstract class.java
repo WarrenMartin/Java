3f2a17eec5752584944cd2c575eb4f41bc7e01d1
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-04-14 11:02:07 -0400

    Update Abstract class.java

--- Abstract/Abstract class.java
+++ Abstract/Abstract class.java
@@ -8,3 +8,8 @@ abstract public class ATM{
       System.out.println("I am xyz ATM");
   }
 }
+
+/*
+Note that withdraw and deposit are abstract method that means it is just an idea or an specification
+concrete class is the disp in the above example
+also disp(){}; this is also a considered a concrete method
