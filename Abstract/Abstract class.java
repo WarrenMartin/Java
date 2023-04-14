5fc9c12137d7675912f76b806edcc36cc1b8b215
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-04-14 10:57:41 -0400

    Create Abstract class.java

---
+++ Abstract/Abstract class.java
@@ -0,0 +1,10 @@
+abstract public class ATM{
+  
+  public abstract void withdraw();
+  
+  public abstract void deposit();
+  
+  public void disp(){
+      System.out.println("I am xyz ATM");
+  }
+}
