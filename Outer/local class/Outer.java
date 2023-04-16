e09fa931a57b56fb348b9a8806db3c1ef70577e1
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-04-16 06:27:12 -0400

    Update Outer.java

--- Outer/local class/Outer.java
+++ Outer/local class/Outer.java
@@ -4,3 +4,37 @@ As a local only final (final static) modifier is applicable.
 Its scope is limited to method.
 */
 
+public class OuterLocal{
+	private int data=10;
+	
+	public void display(){
+	//since inside method so inner class is local to the method itself, it cant have access specifier
+		
+		
+		class Local{
+			void msg(){
+				
+				System.out.println(data);
+			}
+		} //class def-end
+		
+	// you have to create the instance of local class inside a method only otherwise you cannot access it
+		
+		Local l=new Local();
+		l.msg();
+		
+	} //Method End
+
+	
+	
+	
+	public static void main(String[] args){
+		
+		OuterLocal out=new OuterLocal();
+		
+		out.display();
+	
+	}
+
+
+}
