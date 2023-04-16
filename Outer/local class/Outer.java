8162e1110f51fbc2fd057ca101ae991e534c4a6b
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-04-16 06:17:09 -0400

    Create Outer.java

---
+++ Outer/local class/Outer.java
@@ -0,0 +1,6 @@
+/*
+When it is a local it cant have access modifier, because it is local that means no private,public,protected  etc.
+As a local only final (final static) modifier is applicable.
+Its scope is limited to method.
+*/
+
