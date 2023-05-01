2db009392328959e62fe6bb0f7355ba2f44c1687
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-05-01 04:03:34 -0400

    Create PuttingArrayInsideList.java

---
+++ CollectionFrameWork/Set/PuttingArrayInsideList.java
@@ -0,0 +1,34 @@
+  import java.util.*;
+
+public class LinkListDemo {
+    public static void main(String[] args) {
+        // LinkedList<String> ll = new LinkedList();
+        // ll.add("Hyderabad");
+        // ll.add("asdf");
+        // ll.add("pune");
+        // ll.add("Bombay");
+        // System.out.println(ll);
+
+        String[] arr={"Kolkata","Mohali","Noida"};
+        List<String> al=Arrays.asList(arr);
+        System.out.println(al);
+
+
+
+
+
+
+
+
+    //     ListIterator<String> litr = ll.listIterator();
+    //     while (litr.hasNext()) {
+    //         String city = litr.next();
+    //         System.out.println(city);
+	// 		if(city.equals("Bombay")){
+	// 			litr.set("Mumbai");
+	// 		}
+    //     }
+
+	// 	System.out.println(ll);
+    }
+}
