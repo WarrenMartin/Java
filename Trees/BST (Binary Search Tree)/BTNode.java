7693f5dbfce4930ba76d75b93ff003c09a1fd2bf
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-05-29 13:26:55 +0530

    Create BTNode.java

---
+++ Trees/BST (Binary Search Tree)/BTNode.java
@@ -0,0 +1,14 @@
+public class BTNode {
+
+    public int data;
+    public BTNode leftchild;
+
+    public BTNode rightchild;
+
+    public BTNode(int data){
+        this.data=data;
+        leftchild=null;
+        rightchild=null;
+    }
+
+}
