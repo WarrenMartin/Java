d5aba1016bdc0844d1c3b056dbecee9b59a2d225
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-08-02 08:50:05 +0530

    Create GenericWildCardDemo.java

---
+++ CollectionFrameWork/Generic/GenericWildCardDemo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WildCardDemo {
+
+    public static void main(String[] args) {
+        List<Integer> ints = new ArrayList<>();
+        ints.add(3);
+        ints.add(4);ints.add(5);
+
+        double sum=sum(ints);
+        System.out.println("Sum of ints="+sum);
+
+    }
+
+    public static double sum(List<? extends Number> list){
+        double sum=0;
+        for(Number n: list){
+            sum+= n.doubleValue();
+        }
+        return  sum;
+    }
+}
